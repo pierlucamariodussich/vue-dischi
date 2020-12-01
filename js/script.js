98edb1674a484ee40681b28b1c8b8cced77e6425
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-12-01 18:35:02 +0100

    dischi + bonus

--- js/script.js
+++ js/script.js
@@ -4,25 +4,30 @@ let myApp = new Vue ({
 
   data:{
      cds:[],
+     genreInput:'All',
   },
   mounted: function(){
 
-
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
     .then((result) =>{
-      this.cds.push(result.data.response);
+
+      this.cds = (result.data.response);
+        console.log(this.cds)
 
       });
 
-    console.log(this.cds)
+  },
+
+  computed:{
+  cdsFiltered: function(){
+      return this.cds.filter(e =>{
+        if(this.genreInput == "All"){
+          return true
+        } else {
+          return e.genre.toLowerCase().includes(this.genreInput.toLowerCase())
+        }
+      });
+    }
   }
-
-
-
-
-
-
-
-
 
 });
